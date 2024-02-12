e2a60f96ae8acbf58bc99002311df7be0372a032
Roddasb <51865663+Roddasb@users.noreply.github.com>
2024-02-11 23:11:04 -0600

    Add files via upload

---
+++ Assignment/Student_Performace_Project/Results/Student_Performace_Analysis_Results.py
@@ -0,0 +1,13 @@
+WARNING:matplotlib.legend:No artists with labels found to put in legend.  Note that artists whose label start with an underscore are ignored when legend() is called with no argument.
+
+
+
+T-Statistic (Math): 5.383245869828983
+P-Value (Math): 9.120185549328822e-08
+T-Statistic (Reading): -7.959308005187657
+P-Value (Reading): 4.680538743933289e-15
+T-Statistic (Writing): -9.979557910004507
+P-Value (Writing): 2.019877706867934e-22
+<ipython-input-49-1dec585cfd98>:65: FutureWarning: The default value of numeric_only in DataFrame.corr is deprecated. In a future version, it will default to False. Select only valid columns or specify the value of numeric_only to silence this warning.
+  sns.heatmap(data.corr(), annot=True, cmap="coolwarm")
+
